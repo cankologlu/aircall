545fbdae6fd396e96ac86bb0644a784373057d00
Can Kologlu <mckologlu@gmail.com>
2023-07-23 23:22:22 -0400

    fix-date-flow

---
+++ src/mockData/mockData.js
@@ -0,0 +1,48 @@
+const mockData = [
+  {
+    direction: "outbound",
+    from: 100000,
+    to: 200000,
+    via: 30000000,
+    duration: 0,
+    call_type: "missed",
+    is_archived: false,
+    id: "6393bb5469073dc45849ca7a",
+    created_at: "2022-12-09T22:48:52.789Z",
+  },
+  {
+    direction: "inbound",
+    from: 100001,
+    to: 200002,
+    via: 30000003,
+    duration: 10,
+    call_type: "answered",
+    is_archived: false,
+    id: "6393bb7b69073dc45849ca7c",
+    created_at: "2022-12-09T22:49:31.911Z",
+  },
+  {
+    direction: "inbound",
+    from: 100001,
+    to: 200001,
+    via: 30000001,
+    duration: 0,
+    call_type: "missed",
+    is_archived: false,
+    id: "639737ac587edc08100c026f",
+    created_at: "2022-12-12T14:16:12.721Z",
+  },
+  {
+    direction: "inbound",
+    from: 100002,
+    to: 200002,
+    via: 30000002,
+    duration: 20,
+    call_type: "voicemail",
+    is_archived: true,
+    id: "63973961362d5c09cd79364a",
+    created_at: "2022-12-12T14:23:29.409Z",
+  },
+];
+
+export default mockData;
